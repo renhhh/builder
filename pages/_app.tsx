763ff944f8b957efb72a9c999a54150d77a0d72e
ren hao <ren.hao@bitget.com>
2022-08-22 18:26:21 +0800

    initial project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+import { builder } from '@builder.io/react'
+import builderConfig from '@config/builder'
+builder.init(builderConfig.apiKey)
+
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
